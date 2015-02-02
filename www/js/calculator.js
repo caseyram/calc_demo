d630a81dce7fb2d2314fbe0b169bd54e782d028d
Casey Ramotowski <cramotowski@gmail.com>
2015-02-01 21:41:15 -0500

    After reviewing the interface, the Calculator class was reworked to include additional assumed behavior.
    Included decimal handling, multiple operand storage, continuous execution of the last operation.
    Removed misunderstood functionality and tests.

--- www/js/calculator.js
+++ www/js/calculator.js
@@ -2,21 +2,89 @@ define([], function() {
 
   return function Calculator() {
 
-    this.add = function(x, y) {
-      return x + y;
+    var _operandLeft;
+    var _operandRight;
+    var _result;
+    var _operation;
+
+    function clear() {
+      _operandLeft = "0";
+      _operandRight = "0";
+      _operation = "";
+      return _result = "0";
+    }
+
+    clear();
+
+    function setNumber(x) {
+      var target = "";
+
+      if(_operation == "") {
+        target = _operandLeft;
+      } else {
+        target = _operandRight
+      }
+
+      if(target != "0") {
+        // Append to the previous number
+        target = target + "" + x;
+      } else {
+        // If we're at 0 then we'll just use the input instead
+        target = "" + x;
+
+        // Unless our input is a decimal
+        if(x == ".") {
+          target = "0.";
+        }
+      }
+
+      if(_operation == "") {
+        return _result = _operandLeft = target;
+      } else {
+        return _result = _operandRight = target;
+      }
+    }
+
+    // Result will return the latest entered or
+    this.result = function() {
+      return _result;
     };
 
-    this.subtract = function(x, y) {
-      return x - y;
-    };
+    this.input = function(x) {
+      var number = Number(x);
 
-    this.multiply = function(x, y) {
-      return x * y;
-    };
+      // Have we a number?
+      if(number < 10 && number > -1) {
+        return setNumber(x);
+      }
 
-    this.divide = function(x, y) {
-      return x / y;
-    };
+      // A decimal?
+      if(x == ".") {
+        // Only 1 allowed
+        if(!this.result().match(/\./)) {
+          return setNumber(x);
+        }
+      }
 
+      // Clear?
+      if(x.match(/^c$/i)) {
+        return clear();
+      }
+
+      // Add Subtract Multiply or Divide?
+      if(x.match(/^[\+\-x*\/]$/i)) {
+        if(x.match(/x/)) {
+          x = "*";
+        }
+        return _operation = x;
+      }
+
+      // Execute
+      if(x == "=") {
+        // A fun way to simplify how we perform the operation
+        // eval lets us create a string and run it on demand
+        return _operandLeft =_result = eval(_operandLeft + _operation + _operandRight);
+      }
+    };
   };
 });
--- www/js/calculator_spec.js
+++ www/js/calculator_spec.js
@@ -8,40 +8,120 @@ requirejs.config({
 
 describe('Calculator', function() {
   var calculator;
+  var Calculator;
 
   before(function(done) {
-    requirejs(['calculator'], function(Calculator) {
-      calculator = new Calculator();
+    requirejs(['calculator'], function(calculatorjs) {
+      Calculator = calculatorjs;
       done();
     });
   });
 
-  describe('add', function() {
-    it('should return the sum of two integers', function() {
-      assert.equal(4, calculator.add(1, 3));
-    });
+  beforeEach(function() {
+    calculator = new Calculator();
   });
 
-  describe('subtract', function() {
-    it('should return the difference of two integers', function() {
-      assert.equal(2, calculator.subtract(3, 1));
-    });
-  });
-
-  describe('multiply', function() {
-    it('should return the product of two integers', function() {
-      assert.equal(6, calculator.multiply(3, 2));
-    });
-  });
-
-  describe('divide', function() {
-    it('should return the quotient of two integers which produces no remainder', function() {
-      assert.equal(6, calculator.divide(24, 4));
+  // Our Calculator Should Take Numerical Input
+  describe('input', function() {
+    it('should accept input commands for all numbers and return each one as the result', function() {
+      var inputCalculator;
+      for(var i = 0; i < 10; i++) {
+        inputCalculator = new Calculator();
+        inputCalculator.input(i);
+        assert.equal(i, inputCalculator.result());
+      }
     });
 
-    it('should return the quotient of two integers which produces a remainder', function() {
-      // here we round to two decimal places to simplify equality consistency
-      assert.equal(3.43, calculator.divide(24, 7).toFixed(2));
+    it('should accept multiple numbers and increase the result by appending each number to the previous', function() {
+      calculator.input(1);
+      calculator.input(2);
+      assert.equal("12", calculator.result());
     });
-  });
+
+    it('should accept numbers and decimal as valid input types', function() {
+      calculator.input("L");
+      assert.equal("0", calculator.result());
+
+      calculator.input(8);
+      assert.equal("8", calculator.result());
+
+      calculator = new Calculator();
+      calculator.input(".");
+      assert.equal("0.", calculator.result());
+    });
+
+    it('should only accept 1 decimal', function() {
+      calculator.input(".");
+      calculator.input("5");
+      calculator.input(".");
+      assert.equal("0.5", calculator.result());
+    });
+
+    // Clear Functionality
+    it('should clear the result when passed the letter c', function() {
+      calculator.input("5");
+      calculator.input('c');
+      assert.equal('0', calculator.result());
+    });
+
+    // Operands
+    it('should execute the add command when passed [number, "+", number, "="]', function() {
+      calculator.input("5");
+      calculator.input("+");
+      calculator.input("4");
+      calculator.input("=");
+      assert.equal("9", calculator.result());
+    });
+
+    it('should execute the subtract command when passed [number, "-", number, "="]', function() {
+      calculator.input("5");
+      calculator.input("-");
+      calculator.input("4");
+      calculator.input("=");
+      assert.equal("1", calculator.result());
+    });
+
+    it('should execute the multiply command when passed [number, "x", number, "="]', function() {
+      calculator.input("5");
+      calculator.input("x");
+      calculator.input("4");
+      calculator.input("=");
+      assert.equal("20", calculator.result());
+    });
+
+
+    it('should execute the multiply command when passed [number, "*", number, "="]', function() {
+      calculator.input("5");
+      calculator.input("*");
+      calculator.input("4");
+      calculator.input("=");
+      assert.equal("20", calculator.result());
+    });
+
+    it('should execute the divide command when passed [number, "/", number, "="]', function() {
+      calculator.input("5");
+      calculator.input("/");
+      calculator.input("4");
+      calculator.input("=");
+      assert.equal("1.25", calculator.result());
+    });
+
+    it('should show the current number when in the middle of inputing an operation', function() {
+      calculator.input("5");
+      calculator.input("/");
+      assert.equal("5", calculator.result());
+      calculator.input("4");
+      assert.equal("4", calculator.result());
+    });
+
+    it('should continue executing the last operation on subsequent = inputs [number, "+", number, "=", "="]', function() {
+      calculator.input("5");
+      calculator.input("+");
+      calculator.input("4");
+      calculator.input("=");
+      assert.equal("9", calculator.result());
+      calculator.input("=");
+      assert.equal("13", calculator.result());
+    });
+  })
 });
